8507bcea8abf127cf8119f379b85cf5a577f8350
임성룡 <ryongseong@ryong-2.local>
2023-08-03 15:42:28 +0900

    python

--- 인사고과/인사고과1.py
+++ 인사고과/인사고과1.py
@@ -1,3 +1,22 @@
 def solution(scores):
+    wanho_score = scores.pop(0)
+
+    sco = []
+
+    for score in scores:
+        a, b = score
+        
+
+    
     answer = 0
     return answer
+
+
+
+
+
+
+
+
+print(solution([[2,2], [1, 4], [3,2], [3, 2], [2, 1]]))
+# answer => 4
