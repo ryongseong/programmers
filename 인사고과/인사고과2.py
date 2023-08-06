5bb82f38ddd206abe0eabef740ec6e83b74165c5
ryongseong <xmssnsk2119@gmail.com>
2023-08-06 17:38:20 +0900

    python

---
+++ 인사고과/인사고과2.py
@@ -0,0 +1,17 @@
+def solution(scores):
+    answer = 1
+
+    wanho = scores.pop(0)
+    wanho_sum = sum(wanho)
+
+    for a, b in scores:
+        if wanho_sum < a + b:
+            answer += 1
+        
+        if wanho[0] < a and wanho[1] < b:
+            return -1
+    
+
+    return answer
+
+print(solution([[7, 1], [6, 6], [5, 4], [5, 4], [6, 6]]))  # 출력: 3
