2c03beadfc8a1c9330720a922f2cffa8b3e84766
임성룡 <ryongseong@ryong-2.local>
2023-08-20 20:31:56 +0900

    무지의 먹방 라이브

--- 무지의 먹방 라이브/무지의먹방라이브_solution2.py
+++ 무지의 먹방 라이브/무지의먹방라이브_solution2.py
@@ -4,12 +4,17 @@ def solution(food_times, k):
     for i in range(len(food_times)):
         food_list.append([i, food_times[i]])
 
+    # 시간의 합이 k초보다 작다면 k초 후에 먹을 음식이 없기에 -1을 반환
     if sum(food_times) <= k:
         return -1
 
-    food_list = sorted(food_list, key = lambda x:x[1])
+    # 시간을 기준으로 오름차순
+    food_list.sort(key= lambda x:x[1])
 
+    # 바퀴를 돌 때 시간이 가장 작은 음식을 다 먹는데 걸리는 시간
     delTime = food_list[0][1]*len(food_list)
+
+    # 음식 번호
     i = 1
     
     while delTime < k:
